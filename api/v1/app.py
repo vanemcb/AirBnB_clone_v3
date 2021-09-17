ee236a34f5c2bd0b059c97e7adf3aefa89ebb2e7
vanemcb <vanem2201@gmail.com>
2021-09-17 17:36:06 -0500

    handler for 404 errors that returns a JSON-formatted 404 status code response created

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ app module  """
 
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -14,6 +14,11 @@ app.register_blueprint(app_views)
 def teardown(exception):
     """Method to handle the teardown of the application"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
